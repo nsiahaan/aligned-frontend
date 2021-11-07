70ff0c2ee8720d5687dfb6726a243de140a6d0a2
nkuma23 <napa1922@gmail.com>
2021-11-06 17:19:27 -0700

    Added routes for firebase

--- aligned/firebase.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("~/CSCode/aligned/key.json")
-firebase_admin.initialize_app(cred)
-default_app = initialize_app(cred)
-db = firestore.client()
-users_ref = db.collection('users')
-
--- aligned/firebaseDB.py
+++ aligned/firebaseDB.py
@@ -1,14 +1,4 @@
-"""
-Aligned 
-
-aligned.routes
-This module implements the routes for Aligned
-"""
-from flask import render_template, url_for, redirect, request, jsonify
-from aligned import app
-
-
-import firebase_admin, firebase
+import firebase_admin
 from flask import jsonify
 from firebase_admin import credentials, firestore
 
@@ -17,14 +7,9 @@ default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 users_ref = db.collection('users')
 
-@app.route('/')
-@app.route("/home")
-def home():
-    #Home Page
-    return render_template('home.html', title="Home")
 
-@app.route('/add', methods=['POST'])
-def create():
+
+def create(request):
     """
         create() : Add document to Firestore collection with request body
         Ensure you pass a custom ID as part of json body in post request
@@ -37,28 +22,9 @@ def create():
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/list', methods=['GET'])
-def read():
-    """
-        read() : Fetches documents from Firestore collection as JSON
-        todo : Return document that matches query ID
-        all_todos : Return all documents
-    """
-    try:
-        # Check if ID was passed to URL query
-        todo_id = request.args.get('id')    
-        if todo_id:
-            todo = firebase.users_ref.document(todo_id).get()
-            return jsonify(todo.to_dict()), 200
-        else:
-            all_todos = [doc.to_dict() for doc in users_ref.stream()]
-            return jsonify(all_todos), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
 
 
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
+def update(request):
     """
         update() : Update document in Firestore collection with request body
         Ensure you pass a custom ID as part of json body in post request
@@ -71,7 +37,6 @@ def update():
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/delete', methods=['GET', 'DELETE'])
 def delete(request):
     """
         delete() : Delete a document from Firestore collection
@@ -83,16 +48,3 @@ def delete(request):
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
-    
-
-
-
-
-# @app.route('/addPic', methods=['POST','PUT'])
-# def addPic():
-#     # add profile pic linked to user
-#     try:
-#         todo_id = request.args.get('id')
-
-#     except Exception as e:
-#         return f"An Error Occured: {e}"
