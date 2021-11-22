02b617f75fe2aaa5567e7b1f7268a91225b5765e
Nikita Siahaan <niksiahaan@gmail.com>
2021-11-22 02:52:38 -0800

    updated Landing page

--- client/src/routes/Landing.svelte
+++ client/src/routes/Landing.svelte
@@ -1,18 +1,24 @@
 <section>
+    <div class="nav-area">
+        <button type="button" class="btn btn-outline-dark">Log In</button>
+    </div>
+
     <div class="container1">
         <!--<h1>aligned</h1>-->
         <!-- svelte-ignore a11y-missing-attribute -->
         <img src="/logo.png" class="logo"/>
         <h3>Calculated connections.</h3>
         <p>It's like dating, but better. See what the skies may bring you as the stars align...</p>
+        <div class="buttons">
+            <button type="button" class="btn btn-outline-dark">Sign Up</button>
+        </div>
     </div>
-
 </section>
 
 <style>
     :global(body) {
         background-color: pink;
-        background-image: url('temp-bgd.jpg');
+        background-image: url("https://drive.google.com/file/d/1jNPUVDQKxyFmE7K2ivNhA3jRA3LZ4YJI/view");
     } 
     div.container1 {
         color: black;
@@ -24,5 +30,12 @@
     .logo {
         height: 200px;
         width: auto
+    }
+    .nav-area {
+        float: right;
+        padding-right: 5%
+    }
+    .buttons {
+        padding-top: 25px
     }
 </style>
