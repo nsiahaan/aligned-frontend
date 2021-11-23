002ce70beaf5912e2bdc4e17cafc4e3e823bb773
ME <56805453+nmark00@users.noreply.github.com>
2021-11-23 08:06:36 -0800

    Prevent access to pages unless authenticated

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,12 +1,18 @@
 <script>
     import Nav from '$lib/header/Nav.svelte'
+    import Login from './Login.svelte'
     import {page} from '$app/stores';
+    import { youser, isAuthenticated } from '../store.js'
     export let page_tracker = $page.path.replace(/[^\w\s]/gi, '');
     
 </script>
 
 <Nav bind:page_tracker={page_tracker} />
+{#if !$isAuthenticated}
+<Login/>
 
+{:else}
 <main>
     <slot />
 </main>
+{/if}
